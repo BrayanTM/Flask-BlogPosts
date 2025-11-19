47d7a77dc7c97ef5da30d159920f28ec06e227fa
Brayan Tebelan <brayantebelan@gmail.com>
2025-11-19 16:56:01 -0600

    feat: Agregar configuración de base de datos y modelos de usuario y publicación

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
+    
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
