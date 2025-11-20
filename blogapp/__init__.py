0719c126b4c21fcf9da42f72921a5e172057c25e
Brayan Tebelan <brayantebelan@gmail.com>
2025-11-20 00:11:30 -0600

    feat: Módulo auth finalizado - Se agregó la lógica completa de auth (register, login, profile). - Se agrego Cloudinary como medio de almacenamiento para los archivos multimedia de los usuarios. - Las vistas de register, login y profile estan completas.

--- blogapp/__init__.py
+++ blogapp/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from blogapp import home, auth, post
 from blogapp.db_con import db
-from .models import User, Post
+from flask_migrate import Migrate
+import cloudinary
+import os
+
+
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object('config.Config')
+    cloudinary.config(cloudinary_url=os.getenv("CLOUDINARY_URL"))
+
+    app.config.from_object("config.Config")
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(home.home_bp)
     app.register_blueprint(auth.auth_bp)
--- blogapp/post.py
+++ blogapp/post.py
@@ -10,16 +10,16 @@ post_bp = Blueprint('post', __name__, url_prefix='/post')
 @post_bp.route('/posts')
 @login_required
 def posts():
-    return 'pagina de blogposts'
+    return render_template('admin/posts.html')
 
 
 @post_bp.route('/create')
 @login_required
 def create():
-    return 'pagina de blogposts'
+    return render_template('admin/create.html')
 
 
 @post_bp.route('/update/<int:post_id>')
 @login_required
 def update(post_id):
-    return f'pagina para actualizar el blogpost {post_id}'
+    return render_template('admin/update.html')
